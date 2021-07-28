1e9e08b10247903a3abe134a2abf9ec12e8f758f
Niranjan Rathor <84126253+NiranjanRathor47@users.noreply.github.com>
2021-07-28 12:07:06 +0530

    Add files via upload

---
+++ basicdatatypepart5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    
+    int a;
+    long b;
+    char c ;
+    float d;
+    double e;
+    
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cin>>e;
+    
+    
+    cout<< setprecision(20) << a <<endl;
+    cout<< setprecision(20) << b <<endl;
+    cout<< setprecision(20) << c <<endl;
+    cout<< setprecision(20) << d <<endl;
+    cout<< setprecision(20) << e <<endl;
+   
+    
+    
+    return 0;
+}
