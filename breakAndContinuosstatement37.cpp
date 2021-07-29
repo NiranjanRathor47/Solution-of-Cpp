7bec63f78f1c1eebb3954fe6ec6ff47ee53b3290
Niranjan Rathor <84126253+NiranjanRathor47@users.noreply.github.com>
2021-07-29 11:56:20 +0530

    Add files via upload

---
+++ breakAndContinuosstatement37.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+     /* for (int i = 0; i < 4; i++)
+      {
+          if(i==2)
+          break ;
+          cout<<i<<endl;
+      }
+    */
+   for (int i = 0; i < 40; i++)
+      {
+          if(i==2)
+          continue ;
+          cout<<i<<endl;
+      }
+    return 0;
+}
