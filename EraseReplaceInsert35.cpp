bbbba335c3a81726fab309f04e43f697858bcddd
Niranjan Rathor <84126253+NiranjanRathor47@users.noreply.github.com>
2021-08-10 07:02:01 +0530

    Add files via upload

---
+++ EraseReplaceInsert35.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main () 
+{
+    string s1("Hey my name is Niranjan and learn to code");
+
+   /* cout<<s1<<endl;
+    s1.erase(24);
+    cout<<s1<<endl; */
+
+   /* cout<<s1<<endl;
+    s1.replace(15,8,"Niranjan Rathor");
+    cout<<s1<<endl; */
+
+    cout<<s1<<endl;
+    s1.insert(15,"great ");
+    cout<<s1<<endl;
+
+    return 0;
+}
