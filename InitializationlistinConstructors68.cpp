790552a49ba10caee671bd4af75ce36f6ac08653
Niranjan Rathor <84126253+NiranjanRathor47@users.noreply.github.com>
2021-08-17 08:00:41 +0530

    Add files via upload

---
+++ InitializationlistinConstructors68.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class test
+{
+    int a, b;
+public:
+    // Test(int i, int j) : a(i), b(j)
+    // Test(int i, int j) : a(i), b(i+j)
+    // Test(int i, int j) : a(i), b(2 * j)
+    // Test(int i, int j) : a(i), b(a + j)
+    // Test(int i, int j) : b(j), a(i+b) -->RED Flag this will create problems because a will be initialized first
+    test(int i, int j)
+    {
+        a = i;
+        b = j;
+        cout << "Constructor executed" << endl;
+        cout << "Value of a is " << a << endl;
+        cout << "Value of b is " << b << endl;
+    }
+};
+
+int main()
+{
+    test(5, 8);
+    return 0;
+}
